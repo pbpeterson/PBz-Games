ee3a2f6a5dfa7d0ef3c3c7808311be0a0bc5787e
Peterson Pereira <petersonbozza7@gmail.com>
2021-08-05 08:55:53 -0300

    add Banner

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,26 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
+describe('<Banner />', () => {
+  it('should render Banner correctly', () => {
+    renderWithTheme(<Banner {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /Defy death/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Play the new crashlands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByLabelText(/Defy death/i)).toBeInTheDocument()
+  })
+})
