9173e4f49fb310790e1f2c1be0d798221b141275
Peterson Pereira <petersonbozza7@gmail.com>
2021-08-08 15:32:54 -0300

    add Banner

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -23,4 +23,24 @@ describe('<Banner />', () => {
     ).toBeInTheDocument()
     expect(screen.getByLabelText(/Defy death/i)).toBeInTheDocument()
   })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({ backgroundColor: '#3CD3C1' })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
+  })
 })
