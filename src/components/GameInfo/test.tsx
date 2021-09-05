c6b1ed80ddea8ce4ab652db3cbeb548329fbf793
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-05 01:32:55 -0300

    added GameInfo and its tests

---
+++ src/components/GameInfo/test.tsx
@@ -0,0 +1,37 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameInfo from '.'
+
+const props = {
+  title: 'My game title',
+  description: 'My game description',
+  price: '210.00'
+}
+
+describe('<GameInfo />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    //esperar por um heading (title)
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    //esperar por description
+    expect(screen.getByText(props.description)).toBeInTheDocument()
+    //esperar pelo price
+    expect(screen.getByText(`$${props.price}`)).toBeInTheDocument()
+  })
+  it('should render buttons', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    //esperar por button add to cart
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /add to cart/i })
+    ).toBeInTheDocument()
+    //esperar por button wishlist
+  })
+})
