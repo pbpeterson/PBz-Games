0f94d2288a4fbaa0450c9b1b3a5310f87f778513
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-08 02:25:13 -0300

    Added Divider

---
+++ src/components/Divider/test.tsx
@@ -0,0 +1,28 @@
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import { Divider } from '.'
+
+describe('<Divider />', () => {
+  it('should render the correctly', () => {
+    const { container } = renderWithTheme(<Divider />)
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+.c0 {
+  margin: 4.8rem auto 3.2rem;
+  height: 0 1rem;
+  background: rgba(181,181,181,0.3);
+  border: 0;
+}
+
+@media (min-width:768px) {
+  .c0 {
+    margin: calc(4.8rem * 2.5) auto 4.0rem;
+  }
+}
+
+<div
+  class="c0"
+/>
+`)
+  })
+})
