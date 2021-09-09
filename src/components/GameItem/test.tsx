8b817158a44858a149fa9ce2abbd9b1983c082b6
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-09 12:53:48 -0300

    Added GameItem

---
+++ src/components/GameItem/test.tsx
@@ -0,0 +1,48 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameItem, { PaymentInfoProps } from '.'
+
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 251,00'
+}
+
+describe('<GameItem />', () => {
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: /red dead redemption 2/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByText(props.price)).toBeInTheDocument()
+
+    expect(screen.getByRole('img')).toHaveAttribute('src', props.img)
+  })
+  it('should render the item with download link', () => {
+    const downloadLink = 'https://link'
+
+    renderWithTheme(<GameItem {...props} downloadLink={downloadLink} />)
+
+    expect(
+      screen.getByRole('link', { name: `Get ${props.title} here` })
+    ).toHaveAttribute('href', downloadLink)
+  })
+  it('should render the item payment info', () => {
+    const paymentInfo: PaymentInfoProps = {
+      flag: 'mastercard',
+      img: '/img/cards/master-card.png',
+      number: '**** **** **** 4321',
+      purchaseDate: 'Purchase made on 07/20/2020/ at 14:05'
+    }
+
+    renderWithTheme(<GameItem {...props} paymentInfo={paymentInfo} />)
+    expect(
+      screen.getByRole(/img/i, {
+        name: paymentInfo.flag
+      })
+    ).toHaveAttribute('src', paymentInfo.img)
+  })
+})
