04de186bbaea1077233f20f6cf9f2be9329cb686
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-12 14:55:16 -0300

    Added CartList

---
+++ src/components/CartList/test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import mockItems from './mock'
+
+import CartList from '.'
+
+describe('<CartList />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" />)
+
+    expect(screen.getAllByRole('heading')).toHaveLength(2)
+    expect(screen.getByText('R$ 330,00')).toBeInTheDocument()
+  })
+})
