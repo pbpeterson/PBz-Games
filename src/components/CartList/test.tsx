a87daccb804cead3fb3e06ae460b865eef04aacd
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-19 00:03:39 -0300

    Added Button on CartList

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -12,4 +12,9 @@ describe('<CartList />', () => {
     expect(screen.getAllByRole('heading')).toHaveLength(2)
     expect(screen.getByText('R$ 330,00')).toBeInTheDocument()
   })
+  it('should render the button', () => {
+    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" hasButton />)
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
 })
