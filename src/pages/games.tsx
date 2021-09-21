c06970ec789692a7c716900d33587fe32102b568
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-09-21 03:19:19 -0300

    Added apollo and graphql

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,17 +1,39 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 
-import gamesMock from 'components/GameCardSlider/mock'
 import mockFilterItems from 'components/ExploreSideBar/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QUERY_GAMES } from 'graphql/queries/games'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+    query: QUERY_GAMES,
+    variables: { limit: 9 }
+  })
+
   return {
     props: {
-      games: gamesMock,
+      games: data.games.map((game) => ({
+        title: game.name,
+        developer: game.developers[0] ? game.developers[0].name : 'Unknown',
+        img: game.cover!.url,
+        price: new Intl.NumberFormat('en', {
+          style: 'currency',
+          currency: 'USD'
+        }).format(game.price)
+      })),
       filterItems: mockFilterItems
     }
   }
 }
+
+// title: string
+// developer: string
+// img: string
+// price: string
