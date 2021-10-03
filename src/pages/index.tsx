bf963db544beed8336f4d5edc642ed13fefa3869
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-10-03 02:29:20 -0300

    Added dynamic highlights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
 
-import HighlightMock from 'components/Highlight/mock'
 import { initializeApollo } from 'utils/apollo'
 import { Home as QueryHome } from 'graphql/generated/Home'
 import { QUERY_HOME } from 'graphql/queries/home'
@@ -37,7 +36,13 @@ export async function getStaticProps() {
         price: game.price
       })),
       newGamesTitle: sections?.newGames?.title,
-      mostPopularHighlight: HighlightMock,
+      mostPopularHighlight: {
+        title: sections?.popularGames?.highlight?.title,
+        subtitle: sections?.popularGames?.highlight?.subtitle,
+        backgroundImage: sections?.popularGames?.highlight?.background?.url,
+        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
+        buttonLink: sections?.popularGames?.highlight?.buttonLink
+      },
       mostPopularGames: sections?.popularGames!.games.map((game) => ({
         title: game.name,
         slug: game.slug,
@@ -46,7 +51,13 @@ export async function getStaticProps() {
         price: game.price
       })),
       mostPopularGamesTitle: sections?.popularGames?.title,
-      freeGamesHighlight: HighlightMock,
+      freeGamesHighlight: {
+        title: sections?.freeGames?.highlight?.title,
+        subtitle: sections?.freeGames?.highlight?.subtitle,
+        backgroundImage: sections?.freeGames?.highlight?.background?.url,
+        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
+        buttonLink: sections?.freeGames?.highlight?.buttonLink
+      },
       freeGames: FreeGames.map((game) => ({
         title: game.name,
         slug: game.slug,
