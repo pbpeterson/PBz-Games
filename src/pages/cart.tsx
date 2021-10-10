19b8c5457cc463bbffba9a4d13e7aaaf0b4d4a5f
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-10-10 07:39:35 -0300

    Added recommended section on Cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -2,21 +2,31 @@ import Cart, { CartProps } from 'templates/Cart'
 
 import cardsMock from 'components/PaymentOptions/mock'
 import itemsMock from 'components/CartList/mock'
-import gamesMock from 'components/GameCardSlider/mock'
-import highLightsMock from 'components/Highlight/mock'
+import { initializeApollo } from 'utils/apollo'
+import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
       items: itemsMock,
       total: 'R$ 430,00',
       cards: cardsMock,
-      recommendedGames: gamesMock,
-      recommendedHighlight: highLightsMock
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
+      recommendedHighlight: highlightMapper(
+        data.recommended?.section?.highlight
+      )
     }
   }
 }
