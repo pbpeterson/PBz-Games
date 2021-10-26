85bcd97aca0e7762e1eba1a056493ace46f1a381
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-10-26 02:42:49 -0300

    Fixed query

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -4,7 +4,7 @@ import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export const QUERY_GAMES = gql`
   query QueryGames($limit: Int!, $start: Int, $where: JSON, $sort: String) {
-    games(limit: $limit, start: $start) {
+    games(limit: $limit, start: $start, where: $where, sort: $sort) {
       ...GameFragment
     }
   }
