bd1778bf28d59c13c5d0621b6fd5aff44e83dbdd
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-11-01 05:24:30 -0300

    Added Loader to Cart

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -26,7 +26,16 @@ describe('<CartList />', () => {
     render(<CartList hasButton />, { cartProviderProps })
 
     expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
-  })
+  }),
+    it('should render loading', () => {
+      const cartProviderProps = {
+        ...CartContextDefaultValues,
+        loading: true
+      }
+      render(<CartList hasButton />, { cartProviderProps })
+
+      expect(screen.getByTitle(/loading/i)).toBeInTheDocument()
+    })
   it('should render empty if there are no games', () => {
     render(<CartList hasButton />)
 
