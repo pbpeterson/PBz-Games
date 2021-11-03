445f44d6444a5b481502b3d2f0eef55001921a3d
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-11-03 05:24:15 -0300

    Added Joi, validations and its tests

---
+++ src/utils/validations/index.ts
@@ -0,0 +1,52 @@
+import Joi from 'joi'
+
+const fieldsValidations = {
+  username: Joi.string().min(5).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required(),
+  confirm_password: Joi.string()
+    .valid(Joi.ref('password'))
+    .required()
+    .messages({ 'any-only': 'confirm password does not match with password' })
+}
+
+export function signUpValidate(values: signUpValidateProps) {
+  const schema = Joi.object(fieldsValidations)
+
+  return getFieldsErrors(schema.validate(values, { abortEarly: false }))
+}
+
+export type fieldErrors = {
+  [key: string]: string
+}
+
+function getFieldsErrors(objErrors: Joi.ValidationResult) {
+  const errors: fieldErrors = {}
+
+  if (objErrors.error) {
+    objErrors.error.details.forEach((err) => {
+      errors[err.path.join('.')] = err.message
+    })
+  }
+
+  return errors
+}
+
+type signInValidateProps = {
+  email: string
+  password: string
+}
+
+type signUpValidateProps = {
+  username: string
+} & signInValidateProps
+
+export function signInValidate(values: signInValidateProps) {
+  const { email, password } = fieldsValidations
+
+  const schema = Joi.object({ email, password })
+
+  return getFieldsErrors(schema.validate(values, { abortEarly: false }))
+}
