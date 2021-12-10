739f5d97c4d0f81de9958d31854881d7db3d6efc
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-10 14:55:58 -0300

    Added WishlistButton component

---
+++ src/components/WishlistButton/test.tsx
@@ -0,0 +1,52 @@
+import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
+import { render, screen } from 'utils/test-utils'
+import WishlistButton from '.'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
+const session = { jwt: 'any_jwt', user: { email: 'any_email' } }
+useSession.mockImplementation(() => [session])
+
+describe('<WishlistButton />', () => {
+  it('should render a button to add to wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false
+    }
+    render(<WishlistButton id="1" />, { wishlistProviderProps })
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
+  })
+  it('should render a button to remove from wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => true
+    }
+    render(<WishlistButton id="1" />, { wishlistProviderProps })
+
+    expect(screen.getByLabelText(/remove from wishlist/i)).toBeInTheDocument()
+  })
+  it('should render a button with text', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false
+    }
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    expect(screen.getByText(/add to wishlist/i)).toBeInTheDocument()
+  })
+  it('should not render if not logged', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
+    useSession.mockImplementationOnce(() => [null])
+
+    render(<WishlistButton id="1" hasText />, { wishlistProviderProps })
+
+    expect(screen.queryByText(/add to wishlist/i)).not.toBeInTheDocument()
+  })
+})
