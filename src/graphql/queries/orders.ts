b79985c7b801adbf639652b7613e5ba02bf43f01
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-18 00:23:42 -0300

    Added query orders

---
+++ src/graphql/queries/orders.ts
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client'
+import { GameFragment } from 'graphql/fragments/game'
+
+export const QUERY_ORDERS = gql`
+  query QueryOrders($identifier: ID!) {
+    orders(where: { user: { id: $identifier } }) {
+      id
+      created_at
+      card_brand
+      card_last4
+      total_in_cents
+      games {
+        ...GameFragment
+      }
+    }
+  }
+  ${GameFragment}
+`
