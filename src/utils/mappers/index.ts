7cd5ae78e4d2792184a80b675711ab953a8f3f49
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2021-12-18 00:43:01 -0300

    Added order mapper

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -4,6 +4,7 @@ import {
   Home_sections_freeGames_highlight
 } from 'graphql/generated/Home'
 import { QueryGames_games } from 'graphql/generated/QueryGames'
+import { QueryOrders_orders } from 'graphql/generated/QueryOrders'
 import { QueryWishlists_wishlists_games } from 'graphql/generated/QueryWishlists'
 import formatPrice from 'utils/formatPrice'
 
@@ -57,5 +58,37 @@ export const cartMapper = (games: QueryGames_games[] | undefined) => {
         title: game.name,
         price: formatPrice(game.price)
       }))
+    : []
+}
+
+export const ordersMapper = (orders: QueryOrders_orders[]) => {
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(order.created_at))}`
+          },
+          games: order.games.map((game) => {
+            return {
+              id: game.id,
+              title: game.name,
+              downloadLink:
+                'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+              img: `http://localhost:1337${game.cover?.url}`,
+              price: formatPrice(game.price)
+            }
+          })
+        }
+      })
     : []
 }
