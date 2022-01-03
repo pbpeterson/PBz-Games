6c842423c4d84497b4433e56bab84d3ffd800491
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-03 02:05:21 -0300

    Add cypress and its first structure

---
+++ cypress/integration/example.ts
@@ -0,0 +1,7 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+  it('should go to google', () => {
+    cy.google()
+  })
+})
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    /**
+     * Custom command to visit google page
+     * @example cy.google()
+     */
+
+    google(): Chainable<Window>
+  }
+}
