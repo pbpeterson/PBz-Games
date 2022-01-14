5e6f1ccc903294bce515a32d243e890fa5dfbc67
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-14 13:49:11 -0300

    Get children and textContent inside Game Page

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -1,16 +1,25 @@
 /// <reference path="../support/index.d.ts" />
 
-describe('Game Page', () => { 
-  it('should render render game page sections', () => { 
+describe('Game Page', () => {
+  it('should render render game page sections', () => {
     cy.visit('/game/cyberpunk-2077')
-    
-    cy.getByDataCy('game-info').within(()=> {
-      cy.findByRole('heading', {name: /cyberpunk 2077/i}).should('exist')
+
+    cy.getByDataCy('game-info').within(() => {
+      cy.findByRole('heading', { name: /cyberpunk 2077/i }).should('exist')
       cy.findAllByText(/^Cyberpunk 2077 is an open-world/i).should('exist')
       cy.findAllByText('$59.99').should('exist')
-      cy.findByRole('button', { name: /add to cart/i}).should('exist')
+      cy.findByRole('button', { name: /add to cart/i }).should('exist')
     })
 
-    cy.findAllByRole('button', { name: /thumb \-/i}).should('have.length.gt', 0)
+    cy.findAllByRole('button', { name: /thumb \-/i }).should(
+      'have.length.gt',
+      0
+    )
+
+    cy.getByDataCy('content').within(() => {
+      cy.findByRole('heading', { name: /description/i }).should('exist')
+    })
+
+    cy.getByDataCy('content').children().should('have.length.at.least', 2)
   })
 })
