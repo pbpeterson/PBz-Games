2962c5a4ce33ec489c49bab38eec27d8ff697ed3
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-14 13:38:50 -0300

    Start e2e tests to Game page

---
+++ cypress/e2e/game.ts
@@ -0,0 +1,16 @@
+/// <reference path="../support/index.d.ts" />
+
+describe('Game Page', () => { 
+  it('should render render game page sections', () => { 
+    cy.visit('/game/cyberpunk-2077')
+    
+    cy.getByDataCy('game-info').within(()=> {
+      cy.findByRole('heading', {name: /cyberpunk 2077/i}).should('exist')
+      cy.findAllByText(/^Cyberpunk 2077 is an open-world/i).should('exist')
+      cy.findAllByText('$59.99').should('exist')
+      cy.findByRole('button', { name: /add to cart/i}).should('exist')
+    })
+
+    cy.findAllByRole('button', { name: /thumb \-/i}).should('have.length.gt', 0)
+  })
+})
