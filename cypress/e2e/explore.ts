1aa90fb1061165070d6b3b7e468def909607eeaa
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-15 03:42:04 -0300

    Add sort by price test

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -8,9 +8,11 @@ import {
 } from '../../src/utils/filter/fields'
 
 describe('Explore Page', () => {
+  before(()=> {
+    cy.visit('/games')
+  })
 
   it('should render filters columns', () => {
-    cy.visit('/games')
 
     cy.findByRole('heading', { name: /sort by price/i }).should('exist')
     cy.findByRole('heading', { name: /^price/i }).should('exist')
@@ -21,5 +23,25 @@ describe('Explore Page', () => {
     cy.getFields(platformFields)
     cy.getFields(sortFields)
     cy.getFields(genresFields)
+  })
+
+  it('should render 15 games and show more when show more is clicked', () => {
+    cy.getByDataCy('gamecard').should('have.length', 15)
+    cy.findByRole('button', {name: /show more/i}).click()
+    cy.getByDataCy('gamecard').should('have.length', 30)
+  })
+
+  it('should order by price', () => {
+    cy.findByText(/lowest to highest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Aasc')
+    
+    cy.getByDataCy('gamecard').within(() => {
+      cy.findByText(/free/i).should('exist')
+    })
+    
+    cy.findByText(/highest to lowest/i).click()
+    cy.location('href').should('contain', 'sort=price%3Adesc')
+    cy.findByText(/free/i).should('not.exist')
+
   })
 })
