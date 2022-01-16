06f26e95a79bb2220aad6dafa5045ad0a0c7cff7
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-16 03:03:27 -0300

    Add signIn test

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -1,12 +1,31 @@
 import { CreateUser } from '../support/generate'
 
 describe('User', () => {
-  it('should sign up', () => {
+  it.skip('should sign up', () => {
     const user = CreateUser()
     cy.visit('sign-up')
 
     cy.signUp(user)
 
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+  })
+
+  it('should sign in and log out', () => {
+    cy.visit('/sign-in')
+
+    cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
+    cy.findByPlaceholderText(/password/i).type('123456')
+    cy.findByRole('button', { name: /sign in now/i }).click()
+
+    cy.findByText(/cypress/i)
+      .should('exist')
+      .click()
+
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+
+    cy.findByText(/cypress/i)
+    .should('not.exist')
   })
 })
