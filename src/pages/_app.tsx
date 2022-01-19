18a3bf5d71c65ad7a9c0e42e9b041529d35b7f59
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-19 03:09:04 -0300

    chore: updated title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ function App({ Component, pageProps }: AppProps) {
           <CartProvider>
             <WishlistProvider>
               <Head>
-                <title>Won Games</title>
+                <title>PBz - Games</title>
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
