11ee9eaf7e4641c63f822172f43cb421fb1c09fa
Peterson Pereira Bozza <petersonbozza7@gmail.com>
2022-01-19 02:32:37 -0300

    refactor: change images and SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,7 +27,12 @@ function App({ Component, pageProps }: AppProps) {
                 <link rel="shortcut icon" href="/img/icon-512.png" />
                 <link rel="apple-touch-icon" href="/img/icon-512.png" />
                 <link rel="manifest" href="/manifest.json" />
-                <meta name="theme-color" content="#06092B" />
+                <link
+                  rel="shortcut icon"
+                  href="/img/favicon.png"
+                  type="image/png"
+                />
+                <meta name="theme-color" content="#121212" />
                 <meta
                   name="description"
                   content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
